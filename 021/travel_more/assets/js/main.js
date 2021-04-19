510a5ea2384710924d45df19706a7e4bbfc409b4
Max B.S <maxbsiv@gmail.com>
2021-04-20 02:19:35 +0300

    js test

--- 021/travel_more/assets/js/main.js
+++ 021/travel_more/assets/js/main.js
@@ -1,18 +1,33 @@
-let fixedOffset = $("header").offset().top;
+window.onscroll = function() {myFunction()};
+
+let header = document.getElementById("header");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed");
+    $(".replacer").addClass("show");
+  } else {
+    header.classList.remove("fixed");
+    $(".replacer").removeClass("show");
+  }
+}
+
+// let fixedOffset = $("header").offset().top;
 
 $(function () {
-  $(window).scroll(function () {
-    let fixed = $(".header"),
-      scroll = $(window).scrollTop();
+//   $(window).scroll(function () {
+//     let fixed = $(".header"),
+//       scroll = $(window).scrollTop();
   
-    if (scroll >= fixedOffset) {
-      fixed.addClass("fixed");
-      $(".replacer").addClass("show");
-    } else {
-      fixed.removeClass("fixed");
-      $(".replacer").removeClass("show");
-    }
-  });
+//     if (scroll >= fixedOffset) {
+//       fixed.addClass("fixed");
+//       $(".replacer").addClass("show");
+//     } else {
+//       fixed.removeClass("fixed");
+//       $(".replacer").removeClass("show");
+//     }
+//   });
 
   $(".hamburger, #mobile-menu-shadow").on("click", function () {
     $(".hamburger").toggleClass("is-active");
