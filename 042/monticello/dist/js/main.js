1d0291f245845e8998f4f8a9a0d0345d035add00
MoreusT <62840550+MoreusT@users.noreply.github.com>
2021-06-05 15:37:31 +0300

    fix

--- 042/monticello/dist/js/main.js
+++ 042/monticello/dist/js/main.js
@@ -1,1 +1,28 @@
-console.log('hello');
+window.onscroll = function() {myFunction()};
+
+let header = document.getElementById("header");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+
+$(function () {
+  $(".hamburger, #mobile-menu-shadow").on("click", function () {
+    $(".hamburger").toggleClass("is-active");
+    $("body").toggleClass("open");
+  });
+
+  $(".mob-menu__link").on("click", function () {
+    $(".hamburger").removeClass("is-active");
+    $("body").toggleClass("open");
+  });
+
+  $("#to-top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+  });
+});
